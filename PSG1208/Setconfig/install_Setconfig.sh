f10e9a1d32137e8e3a3b959171585d94eb5da3d9
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-24 13:29:12 +0800

    Update install_Setconfig.sh

--- PSG1208/Setconfig/install_Setconfig.sh
+++ PSG1208/Setconfig/install_Setconfig.sh
@@ -4,5 +4,5 @@ DESTDIR=/opt/rt-n56u
 ROOTDIR=`pwd`
 CONFIG_FILENAME=PSG1208.config
 
-cp -rf ${ROOTDIR}/Setconfig/PHICOMM/. ${DESTDIR}/trunk/configs/boards/PHICOMM
+cp -rf ${ROOTDIR}/PHICOMM/. ${DESTDIR}/trunk/configs/boards/PHICOMM
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/.config"
