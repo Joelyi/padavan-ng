ad07656863b53df6ec1116ad01e34256c1ff451f
Joelyi <37099326+Joelyi@users.noreply.github.com>
2022-07-29 19:07:42 +0800

    Update install.sh

--- Ctools/install.sh
+++ Ctools/install.sh
@@ -5,10 +5,9 @@ ROOTDIR=`pwd`
 
 
 
-echo "--------------开始应用B70补丁----------------------"
- cp -r -f ${ROOTDIR}/B70 ${DESTDIR}/trunk/configs/boards/HIWIFI
- cp -f ${ROOTDIR}/pt_hiwifi_128m.config ${DESTDIR}/trunk/configs/boards/HIWIFI/
- cp -f ${ROOTDIR}/Setconfig/B70.config ${DESTDIR}/trunk/configs/templates/hiwifi
-echo "--------------应用B70补丁结束------------------"
+echo "--------------开始应用PSG1208s补丁----------------------"
+ cp -r -f ${ROOTDIR}/PSG1208s ${DESTDIR}/trunk/configs/boards/PHICOMM
+ cp -f ${ROOTDIR}/Setconfig/PSG1208s.config ${DESTDIR}/trunk/configs/templates/phicomm
+echo "--------------应用PSG1208s补丁结束------------------"
     
  
