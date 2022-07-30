5207c703fd751aa437c3934913f3874053079a0c
Joelyi <37099326+Joelyi@users.noreply.github.com>
2022-07-30 14:21:37 +0800

    Update install_Setconfig.sh

--- Btools/PSG1208s/install_Setconfig.sh
+++ Btools/PSG1208s/install_Setconfig.sh
@@ -2,7 +2,7 @@
 
 DESTDIR=/opt/rt-n56u
 ROOTDIR=`pwd`
-CONFIG_FILENAME=PSG1218.config
+CONFIG_FILENAME=PSG1208s.config
 
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/.config"
 cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/kernel-3.4.x.config"
