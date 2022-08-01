0f4313a1ee2939bb3795997e7edc9d1d31d22541
Joelyi <37099326+Joelyi@users.noreply.github.com>
2022-08-01 09:12:20 +0800

    Update install_Setconfig.sh

--- Btools/PSG1208s/install_Setconfig.sh
+++ Btools/PSG1208s/install_Setconfig.sh
@@ -6,3 +6,5 @@ CONFIG_FILENAME=PSG1208s.config
 
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/.config"
 cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/kernel-3.4.x.config"
+cp -f "${ROOTDIR}/board.h" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/board.h"
+cp -f "${ROOTDIR}/board.mk" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/board.mk"
