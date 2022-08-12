cea490d78914269b62272944024b40bc685a90ec
Joelyi <37099326+Joelyi@users.noreply.github.com>
2022-08-12 16:54:03 +0800

    Update install_Setconfig.sh

--- Btools/PSG1208s/install_Setconfig.sh
+++ Btools/PSG1208s/install_Setconfig.sh
@@ -4,7 +4,9 @@ DESTDIR=/opt/rt-n56u
 ROOTDIR=`pwd`
 CONFIG_FILENAME=PSG1208s.config
 
+    echo "--------------开始复制配置文件----------------------"
 cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/.config"
-cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/kernel-3.4.x.config"
 cp -f "${ROOTDIR}/board.h" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/board.h"
 cp -f "${ROOTDIR}/board.mk" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/board.mk"
+cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1218/kernel-3.4.x.config"
+	      echo "--------------复制配置文件结束------------------"
